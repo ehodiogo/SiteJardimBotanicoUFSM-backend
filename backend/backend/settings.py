d4fe68ddcbcaafe1a8b207bccd9af8930e15d144
Diogo <dapereira@inf.ufsm.br>
2025-07-09 13:36:47 -0300

    feat: static url p funcionar admin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
-
+STATIC_URL = "static/"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
