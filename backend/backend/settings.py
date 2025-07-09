f7b9c3947fc8ab1b32679d749f436f28fc3f5e51
Diogo <dapereira@inf.ufsm.br>
2025-07-09 13:35:44 -0300

    feat: new cors allowed host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -108,6 +108,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "https://sitejardimbotanicoufsm-frontend.onrender.com",
 ]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
